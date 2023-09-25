a153598a76ffb08a59b549e3db95bb243e81fd9e
Julius Figge <julius@figge.eu>
2023-09-25 13:48:12 +0200

    initial revision of code

---
+++ game.js
@@ -0,0 +1,168 @@
+const gameBackground = new Image();
+gameBackground.src = './assets/grass.png';
+
+const honeyPot = new Image();
+honeyPot.src = './assets/honey-pot.svg';
+
+const bee = new Image();
+bee.src = './assets/bee.svg';
+
+let gameCanvas;
+let gameContext;
+
+let ticks = 0;
+const TICKS_PER_FRAME = 10;
+
+const resizeCanvasToFullscreen = () => {
+    if (!gameCanvas) gameCanvas = document.querySelector('#game');
+
+    gameCanvas.width = window.innerWidth;
+    gameCanvas.height = window.innerHeight;
+};
+
+const drawGameBackground = () => {
+    gameContext.fillStyle = gameContext.createPattern(gameBackground, 'repeat');
+    gameContext.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+};
+
+const ensureGameBackground = () => gameBackground.addEventListener('load', () => drawGameBackground());
+
+document.addEventListener('DOMContentLoaded', () => {
+    gameCanvas = document.querySelector('#game');
+    gameContext = gameCanvas.getContext('2d');
+
+    resizeCanvasToFullscreen();
+
+    ensureGameBackground();
+
+    requestAnimationFrame(loop);
+});
+
+const BLOCK_SIZE = 64;
+const VELOCITY = BLOCK_SIZE;
+const getInitialBees = () => ({
+    positionX: BLOCK_SIZE * 10,
+    positionY: BLOCK_SIZE * 10,
+
+    velocityX: VELOCITY,
+    velocityY: 0,
+
+    cellsWithBees: [],
+
+    beesLengthCells: 4
+});
+
+let bees = getInitialBees();
+
+const getInitialHoney = () => ({
+    positionX: BLOCK_SIZE * 20,
+    positionY: BLOCK_SIZE * 20
+});
+
+let honey = getInitialHoney();
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const clearCanvas = () => gameContext.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+const beesAreOutOfBoundsBottom = () => bees.positionX < 0;
+const beesAreOutOfBoundsTop = () => bees.positionX >= gameCanvas.width;
+const beesAreOutOfBoundsLeft = () => bees.positionY < 0;
+const beesAreOutOfBoundsRight = () => bees.positionY >= gameCanvas.height;
+
+const removeBeeFromLastCell = () => bees.cellsWithBees.unshift({x: bees.positionX, y: bees.positionY});
+
+const isBeesListBiggerThanBeeCells = () => bees.cellsWithBees.length > bees.beesLengthCells;
+
+const drawHoney = () => gameContext.drawImage(honeyPot, honey.positionX, honey.positionY, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+
+const beeAteHoney = cell => cell.x === honey.positionX && cell.y === honey.positionY;
+
+const beesRanOverThemself = (cell, i) => cell.x === bees.cellsWithBees[i].x && cell.y === bees.cellsWithBees[i].y;
+
+const resetGame = () => {
+    bees = getInitialBees();
+    honey = getInitialHoney();
+};
+
+const beeCollisionDetection = (index, cell) => {
+    let beeAfterTheCurrent = index + 1;
+    for (let i = beeAfterTheCurrent; i < bees.cellsWithBees.length; i++) {
+        if (beesRanOverThemself(cell, i)) resetGame();
+    }
+};
+
+const placeNewHoney = () => {
+    honey.positionX = getRandomInt(0, 25) * BLOCK_SIZE;
+    honey.positionY = getRandomInt(0, 25) * BLOCK_SIZE;
+};
+
+const drawBee = (cell, index) => {
+
+    gameContext.drawImage(bee, cell.x, cell.y, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+
+    if (beeAteHoney(cell)) {
+        bees.beesLengthCells++;
+        placeNewHoney();
+    }
+
+    beeCollisionDetection(index, cell);
+};
+
+const drawBees = () => bees.cellsWithBees.forEach((cell, index) => drawBee(cell, index));
+
+const shallNotRenderNewFrame = () => ticks < TICKS_PER_FRAME;
+
+const handleWrappingBeesAroundScreen = () => {
+    if (beesAreOutOfBoundsBottom()) bees.positionX = gameCanvas.width - BLOCK_SIZE;
+    else if (beesAreOutOfBoundsTop()) bees.positionX = 0;
+    if (beesAreOutOfBoundsLeft()) bees.positionY = gameCanvas.height - BLOCK_SIZE;
+    else if (beesAreOutOfBoundsRight()) bees.positionY = 0;
+};
+
+const loop = () => {
+    requestAnimationFrame(loop);
+
+    ++ticks;
+    if (shallNotRenderNewFrame()) return;
+    ticks = 0;
+
+    clearCanvas();
+
+    bees.positionX += bees.velocityX;
+    bees.positionY += bees.velocityY;
+
+    handleWrappingBeesAroundScreen();
+
+    removeBeeFromLastCell();
+
+    if (isBeesListBiggerThanBeeCells()) bees.cellsWithBees.pop();
+
+    drawGameBackground();
+    drawHoney();
+    drawBees();
+};
+
+const wasLeftArrowKeyPressedAndNotMovingHorizontally = e => e.key === 'ArrowLeft' && bees.velocityX === 0;
+const wasUpArrowKeyPressedAndNotMovingVertically = e => e.key === 'ArrowUp' && bees.velocityY === 0;
+const wasRightArrowKeyPressedAndNotMovingHorizontally = e => e.key === 'ArrowRight' && bees.velocityX === 0;
+const wasDownArrowKeyPressedAndNotMovingVertically = e => e.key === 'ArrowDown' && bees.velocityY === 0;
+
+const handleKeyboardEvents = e => {
+    if (wasLeftArrowKeyPressedAndNotMovingHorizontally(e)) {
+        bees.velocityX = -VELOCITY;
+        bees.velocityY = 0;
+    } else if (wasUpArrowKeyPressedAndNotMovingVertically(e)) {
+        bees.velocityX = 0;
+        bees.velocityY = -VELOCITY;
+    } else if (wasRightArrowKeyPressedAndNotMovingHorizontally(e)) {
+        bees.velocityX = VELOCITY;
+        bees.velocityY = 0;
+    } else if (wasDownArrowKeyPressedAndNotMovingVertically(e)) {
+        bees.velocityX = 0;
+        bees.velocityY = VELOCITY;
+    }
+};
+
+document.addEventListener('keydown', e => handleKeyboardEvents(e));
+
